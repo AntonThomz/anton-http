00bd6a1dbcd0bb30145aa7cd06b8a5d8b6b2777e
Anton Thomz <antonthomzz@gmail.com>
2024-10-16 11:52:12 +0700

    Delete utils directory

--- utils/utils.js
+++
@@ -1,7 +0,0 @@
-function formatUrl(base, params) {
-    const url = new URL(base);
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-    return url.toString();
-}
-
-module.exports = { formatUrl };
